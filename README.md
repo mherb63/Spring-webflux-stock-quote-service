7dbfb4a7134bd97de889019eacab3a6d8a73098b
Michael Herb <mherb@ciena.com>
2018-05-06 14:44:04 -0400

    create project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![CircleCI](https://circleci.com/gh/mherb63/Spring-webflux-stock-quote-service.svg?style=svg)](https://circleci.com/gh/mherb63/Spring-webflux-stock-quote-service)
 # Spring-webflux-stock-quote-service
---
+++ src/main/java/com/example/springwebfluxstockquoteservice/SpringWebfluxStockQuoteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwebfluxstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxStockQuoteServiceApplication.class, args);
+    }
+}
